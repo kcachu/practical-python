7101963333c372ad870269611413f68def63c69f
kcachu <k.achu1022@gmail.com>
2025-02-24 13:35:46 -0500

    add bounce.py attempt

--- Work/bounce.py
+++ Work/bounce.py
@@ -1,3 +1,11 @@
 # bounce.py
 #
 # Exercise 1.5
+num_of_bounce = 0
+new_height = 100
+
+while(num_of_bounce < 10):
+    new_height = new_height * (3/5)
+    num_of_bounce += 1
+    print(num_of_bounce, round(new_height, 4))
+
