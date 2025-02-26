449fd847957d76613d5e981fb4ae8e4a05e67bf8
kcachu <k.achu1022@gmail.com>
2025-02-26 11:27:04 -0500

    all modifications to mortgage.py including bonus

--- Work/mortgage.py
+++ Work/mortgage.py
@@ -5,16 +5,26 @@
 principal = 500_000.0
 rate = 0.05
 payment =  2684.11
-extra_payment = 1000.0
+extra_payment_start_month =int(input('extra_payment_start_month = '))
+extra_payment_end_month =int(input('extra_payment_end_month = '))
+extra_payment = int(input('Extra payment of = '))
 total_paid = 0.0
-total_months = 0
+month = 0
 
 while principal > 0:
+
+   if principal < payment:
+       payment = principal * (1+rate/12) #could use a walrus but i won't
+
    principal = principal * (1+rate/12) - payment
    total_paid = total_paid + payment
-   if total_months < 12:
+   month += 1
+
+   if month >= extra_payment_start_month and month <= extra_payment_end_month:
        principal -= extra_payment
        total_paid += extra_payment
-   total_months += 1
 
-print('Total paid', total_paid, 'over', total_months, 'months')
+
+   print(month, round(total_paid, 2), round(principal, 2)) 
+
+print('Total paid', round(total_paid, 2), 'over', month, 'months')
